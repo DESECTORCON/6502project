3ad2a0214bc978613f84ea92b33936e22d5ffd3c
Demian Feliox <donotemailme@noreply.com>
2025-08-16 14:07:36 +0900

    Added arduino debugger code

---
+++ 6502debugger/6502debugger.ino
@@ -0,0 +1,30 @@
+#define CLK 2	//Connect to CB1
+#define DATA 3	//Connect to CB2
+#define INT 4	//Connect to IRQB
+
+void setup() {
+	digitalWrite(CLK, HIGH);
+	pinMode(CLK, OUTPUT);
+	pinMode(DATA, INPUT);
+	pinMode(INT, INPUT);
+
+	Serial.begin(115200);
+}
+
+void loop() {
+
+	int data = 0x00;
+	for (int i=0;i < 8;i++)
+	{
+		digitalWrite(CLK, LOW);
+		digitalWrite(CLK, HIGH);
+		int bitin = digitalRead(DATA);
+		data = (data << 1) + bitin;
+	}
+	Serial.println(data, BIN);
+	while (true){	//Wait untill MCU reloads Shift register with new values
+		int check = digitalRead(INT);
+		if (check == HIGH){break;}		
+	}
+
+}
